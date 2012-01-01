9bac6c65aa2a6efc5b48845791c312bc209a5c59
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-01-01 03:50:34 +0000

    - Fixed: Undefined variables - Removed unused Modal Box js and css
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/subscribe-by-email@4834 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ js/sbe.js
@@ -0,0 +1,58 @@
+
+	function SubscribeByEmailCreate() {
+		var SubscriptionEmail = document.getElementById('subscription_email').value;
+		var http = new XMLHttpRequest();
+		if ( SubscriptionEmail != '' && SubscriptionEmail != 'ex: john@hotmail.com' ) {
+			var url = sbe_localized.site_url;
+			var params = "action=sbe_create_subscription&email=" + SubscriptionEmail.replace("+","PLUS");
+			http.open("POST", url, true);
+
+			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			http.setRequestHeader("Content-length", params.length);
+			http.setRequestHeader("Connection", "close");
+			http.onreadystatechange = function() {
+				if(http.readyState == 4) {
+					if (http.status == 200) {
+						jQuery('#subscribe-by-email-msg').html('<div style=\'font-size:20px; padding-bottom:20px;\'><p><center><strong>'+sbe_localized.subscription_created+'</strong></ceneter></p></div>');
+						document.getElementById('subscription_email').value = '';
+					} else {
+						jQuery('#subscribe-by-email-msg').html('<div style=\'font-size:20px; padding-bottom:20px;\'><p><center><strong>'+sbe_localized.already_subscribed+'!</strong></ceneter></p></div>');
+					}
+				}
+			}
+			http.send(params);
+		}
+	}
+	function SubscribeByEmailCancel() {
+		var SubscriptionEmail = document.getElementById('subscription_email').value;
+		var http = new XMLHttpRequest();
+		if ( SubscriptionEmail != '' && SubscriptionEmail != 'ex: john@hotmail.com' ) {
+			var url = sbe_localized.site_url;
+			var params = "action=sbe_cancel_subscription&email=" + SubscriptionEmail.replace("+","PLUS");
+			http.open("POST", url, true);
+
+			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			http.setRequestHeader("Content-length", params.length);
+			http.setRequestHeader("Connection", "close");
+
+			http.onreadystatechange = function() {
+				if(http.readyState == 4) {
+					if (http.status == 200) {
+						jQuery('#subscribe-by-email-msg').html('<div style=\'font-size:20px; padding-bottom:20px;\'><p><center><strong>'+sbe_localized.subscription_cancelled+'</strong></ceneter></p></div>');
+						document.getElementById('subscription_email').value = '';
+					} else {
+						jQuery('#subscribe-by-email-msg').html('<div style=\'font-size:20px; padding-bottom:20px;\'><p><center><strong>'+sbe_localized.failed_to_cancel_subscription+'</strong></ceneter></p></div>');
+					}
+				}
+			}
+			http.send(params);
+		}
+	}
+	jQuery(document).ready(function() {
+		jQuery("#subscribe-by-email-form").submit(function () {
+			SubscribeByEmailCreate();
+			return false;
+		});
+	});
+
+
