9c723f321b856a8636a54a6038b46118e97da970
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-08-29 12:09:09 +0000

    New version
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/subscribe-by-email@10145 f53fe46e-8c02-459b-9fdd-93e75771b278

--- subscribe-by-email.php
+++ subscribe-by-email.php
@@ -4,7 +4,7 @@ Plugin Name: Subscribe by Email
 Plugin URI: http://premium.wpmudev.org/project/subscribe-by-email
 Description: This plugin allows you and your users to offer subscriptions to email notification of new posts
 Author: S H Mohanjith (Incsub), Ignacio (Incsub)
-Version:2.3
+Version:2.4
 Author URI: http://premium.wpmudev.org
 WDP ID: 127
 Text Domain: subscribe-by-email
@@ -123,7 +123,7 @@ class Incsub_Subscribe_By_Email {
 	 * Set the globals variables/constants
 	 */
 	private function set_globals() {
-		define( 'INCSUB_SBE_VERSION', '2.3' );
+		define( 'INCSUB_SBE_VERSION', '2.4' );
 		define( 'INCSUB_SBE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 		define( 'INCSUB_SBE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
